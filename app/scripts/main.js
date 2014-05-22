cc99590a3c6407de79016a6f872cc54405eab59f
Floyd Pink <imbleedingme@googlemail.com>
2014-05-22 13:11:41 -0400

    removed old social icons; url changes; style changes; os mapping for mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,7 +7,7 @@ var fdScripts = (function () {
       tag     : null,
       commits : null
     },
-    getJSON : function(url, callback){
+    getJSON : function (url, callback) {
 
     },
     gaTrack : function (category, action, label, value, nonInteraction) {
@@ -37,21 +37,25 @@ $(function () {
       platforms = {
         'windows'   : {
           name          : 'Windows',
+          commonName    : 'Windows',
           shortName     : 'win',
           installerPath : 'installer/windows/flickrdownloadr-%VERSION%-windows-installer.exe'
         },
         'mac os'    : {
           name          : 'Mac OS X',
+          commonName    : 'Mac OS X',
           shortName     : 'osx',
           installerPath : 'installer/osx/Install%20flickr%20downloadr.app.dmg'
         },
         'linux'     : {
-          name          : '32-bit Linux',
+          name          : 'Linux',
+          commonName    : 'Linux',
           shortName     : 'linux',
           installerPath : 'installer/linux/flickrdownloadr-%VERSION%-linux-installer.run'
         },
         'linux-x64' : {
           name          : '64-bit Linux',
+          commonName    : 'Linux',
           shortName     : 'linux',
           installerPath : 'installer/linux-x64/flickrdownloadr-%VERSION%-linux-x64-installer.run'
         }
@@ -103,61 +107,68 @@ $(function () {
 
   if (currentOsName === 'linux' && Detectizr.os.addressRegisterSize === '64bit') {
     currentOsName = 'linux-x64';
+  } else if (currentOsName === 'android' || currentOsName === 'blackberry') {
+    currentOsName = 'linux';
+  } else if (currentOsName === 'ios') {
+    currentOsName = 'mac os';
   }
 
   $.get('/build.number',
-      function (latestVersion) {
-        var $fdVersion = $('.fd-version');
-        $('.fd-version-text').text(latestVersion);
-        $('#fd-version').fadeIn();
+    function (latestVersion) {
+      var $fdVersion = $('.fd-version');
+      $('.fd-version-text').text(latestVersion);
+      $('#fd-version').fadeIn();
 
-        //set the installer links
-        var currentPlatform = platforms[currentOsName];
-        var $fdDownloadButton = $('#fd-download-button');
-        $fdDownloadButton.attr('onClick', downloadOnClick.replace('%PLATFORM%', currentPlatform.name));
-        $fdDownloadButton.attr('href', currentPlatform.installerPath.replace('%VERSION%', latestVersion));
-        $('#fd-download-platform-name').text(currentPlatform.name);
-        $('#fd-installer').fadeIn();
+      //set the installer links
+      var currentPlatform = platforms[currentOsName];
+      if (!currentPlatform) {
+        currentPlatform = platforms.windows;
+      }
+      var $fdDownloadButton = $('#fd-download-button');
+      $fdDownloadButton.attr('onClick', downloadOnClick.replace('%PLATFORM%', currentPlatform.name));
+      $fdDownloadButton.attr('href', currentPlatform.installerPath.replace('%VERSION%', latestVersion));
+      $('#fd-download-platform-name').text(currentPlatform.name);
+      $('#fd-installer').fadeIn();
 
-        var $fdHiddenSlides = $('#fd-hidden-slides');
-        var $fdScreenshotsCarousel = $('.fd-screenshots-carousel');
-        var $fdScreenshotDialogHeader = $('#fd-screenshot-header-platform');
-        $fdScreenshotDialogHeader.find('span').text(currentPlatform.name);
-        $fdScreenshotDialogHeader.find('a').on('click', function () {
-          $fdScreenshotsCarousel.find('.carousel-inner').append($fdHiddenSlides.find('.item').detach());
-          $fdScreenshotsCarousel.find('.carousel-inner').find('.item').each(function () {
-            var platformName;
-            var item = $(this);
-            if (item.hasClass('win')) {
-              platformName = ' (Windows)';
-            } else if (item.hasClass('osx')) {
-              platformName = ' (Mac OS X)';
-            } else if (item.hasClass('linux')) {
-              platformName = ' (Linux)';
-            }
-            item.find('h4').text(item.find('h4').text() + platformName);
-          });
-          $fdScreenshotDialogHeader.fadeOut();
-          return false;
+      var $fdHiddenSlides = $('#fd-hidden-slides');
+      var $fdScreenshotsCarousel = $('.fd-screenshots-carousel');
+      var $fdScreenshotDialogHeader = $('#fd-screenshot-header-platform');
+      $fdScreenshotDialogHeader.find('span').text(currentPlatform.commonName);
+      $fdScreenshotDialogHeader.find('a').on('click', function () {
+        $fdScreenshotsCarousel.find('.carousel-inner').append($fdHiddenSlides.find('.item').detach());
+        $fdScreenshotsCarousel.find('.carousel-inner').find('.item').each(function () {
+          var platformName;
+          var item = $(this);
+          if (item.hasClass('win')) {
+            platformName = ' (Windows)';
+          } else if (item.hasClass('osx')) {
+            platformName = ' (Mac OS X)';
+          } else if (item.hasClass('linux')) {
+            platformName = ' (Linux)';
+          }
+          item.find('h4').text(item.find('h4').text() + platformName);
         });
+        $fdScreenshotDialogHeader.fadeOut();
+        return false;
+      });
 
-        // set the screenshots for current platform
-        if (currentPlatform.shortName !== 'win') {
-          $fdHiddenSlides.append($fdScreenshotsCarousel.find('.win.item').detach());
-          $fdHiddenSlides.find('.item').removeClass('active');
-          $fdScreenshotsCarousel.find('.carousel-inner').append($fdHiddenSlides.find('.' + currentPlatform.shortName + '.item').detach());
-          $fdScreenshotsCarousel.find('.item').first().addClass('active');
-        }
+      // set the screenshots for current platform
+      if (currentPlatform.shortName !== 'win') {
+        $fdHiddenSlides.append($fdScreenshotsCarousel.find('.win.item').detach());
+        $fdHiddenSlides.find('.item').removeClass('active');
+        $fdScreenshotsCarousel.find('.carousel-inner').append($fdHiddenSlides.find('.' + currentPlatform.shortName + '.item').detach());
+        $fdScreenshotsCarousel.find('.item').first().addClass('active');
+      }
 
-        // get the commit sha for this tag
-        fdScripts.getJSON('https://api.github.com/repos/flickr-downloadr/flickr-downloadr-gtk/git/refs/tags/v' + latestVersion + '?callback=?', function (tagref) {
-          fdScripts.getJSON('https://api.github.com/repos/flickr-downloadr/flickr-downloadr-gtk/git/tags/' + tagref.data.object.sha + '?callback=?', function (tag) {
-            var dateAbbr = $('<abbr></abbr>').attr('title', tag.data.tagger.date).text(' (' + (new Date(tag.data.tagger.date)).toLocaleDateString() + ')').addClass('timeago');
-            $fdVersion.append($('<span></span>').addClass('fd-released').append($('<span></span>').text(' - ')).append(dateAbbr));
-            $('abbr.timeago').timeago();
-          });
+      // get the commit sha for this tag
+      fdScripts.getJSON('https://api.github.com/repos/flickr-downloadr/flickr-downloadr-gtk/git/refs/tags/v' + latestVersion + '?callback=?', function (tagref) {
+        fdScripts.getJSON('https://api.github.com/repos/flickr-downloadr/flickr-downloadr-gtk/git/tags/' + tagref.data.object.sha + '?callback=?', function (tag) {
+          var dateAbbr = $('<abbr></abbr>').attr('title', tag.data.tagger.date).text(' (' + (new Date(tag.data.tagger.date)).toLocaleDateString() + ')').addClass('timeago');
+          $fdVersion.append($('<span></span>').addClass('fd-released').append($('<span></span>').text(' - ')).append(dateAbbr));
+          $('abbr.timeago').timeago();
         });
       });
+    });
 
   Handlebars.registerHelper('strip_sign', function (message, length) {
     var signOffStart = message.indexOf('Signed-off-by:');
@@ -187,37 +198,37 @@ $(function () {
   $(document).on('click', '#getCommits', function () {
     $('#commitsContainer').empty().append($('<div><span class="muted">Loading...</span></div>'));
     fdScripts.getJSON('https://api.github.com/repos/flickr-downloadr/flickr-downloadr-gtk/commits?callback=?',
-        function (response) {
-          var commitsView = '<table>' +
-                  ' <thead>' +
-                  '   <th class=\'fd-commitmessage\'>Message</th>' +
-                  '   <th class=\'fd-commitname\'>Author</th>' +
-                  '   <th class=\'fd-commitdate\'>Date</th>' +
-                  ' </thead>' +
-                  ' <tbody>' +
-                  ' {{#commitsarray}}' +
-                  '   <tr>' +
-                  '     <td class=\'fd-commitmessage\'>' +
-                  '       <span>{{strip_sign commit.message 40}}</span>' +
-                  '       <span>' +
-                  '         <a href=\'{{fix_github_url url}}\' title=\'{{format_date_time commit.author.date}}\' target=\'_blank\'> &raquo;</a>' +
-                  '       </span>' +
-                  '     </td>' +
-                  '     <td class=\'fd-commitname\'>' +
-                  '       <a href=\'{{fix_github_url author.url}}\' target=\'_blank\'>{{strip_sign commit.author.name 15}}</a>' +
-                  '     </td>' +
-                  '     <td class=\'fd-commitdate\'>' +
-                  '       <abbr class=\'timeago\' title=\'{{commit.author.date}}\'>{{format_date commit.author.date}}</abbr>' +
-                  '     </td>' +
-                  '   </tr>' +
-                  ' {{/commitsarray}}' +
-                  ' </tbody>' +
-                  '</table>',
-              commits = { commitsarray : response.data },
-              output = Handlebars.compile(commitsView)(commits);
-          $('#commitsContainer').empty().append(output);
-          $('abbr.timeago').timeago();
-        });
+      function (response) {
+        var commitsView = '<table>' +
+              ' <thead>' +
+              '   <th class=\'fd-commitmessage\'>Message</th>' +
+              '   <th class=\'fd-commitname\'>Author</th>' +
+              '   <th class=\'fd-commitdate\'>Date</th>' +
+              ' </thead>' +
+              ' <tbody>' +
+              ' {{#commitsarray}}' +
+              '   <tr>' +
+              '     <td class=\'fd-commitmessage\'>' +
+              '       <span>{{strip_sign commit.message 40}}</span>' +
+              '       <span>' +
+              '         <a href=\'{{fix_github_url url}}\' title=\'{{format_date_time commit.author.date}}\' target=\'_blank\'> &raquo;</a>' +
+              '       </span>' +
+              '     </td>' +
+              '     <td class=\'fd-commitname\'>' +
+              '       <a href=\'{{fix_github_url author.url}}\' target=\'_blank\'>{{strip_sign commit.author.name 15}}</a>' +
+              '     </td>' +
+              '     <td class=\'fd-commitdate\'>' +
+              '       <abbr class=\'timeago\' title=\'{{commit.author.date}}\'>{{format_date commit.author.date}}</abbr>' +
+              '     </td>' +
+              '   </tr>' +
+              ' {{/commitsarray}}' +
+              ' </tbody>' +
+              '</table>',
+            commits = { commitsarray : response.data },
+            output = Handlebars.compile(commitsView)(commits);
+        $('#commitsContainer').empty().append(output);
+        $('abbr.timeago').timeago();
+      });
   });
 
   $('#fd-slideshow-dialog').find('img').wrap(function () {
